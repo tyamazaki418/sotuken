c76fdfa103e87a9b2e49effa92ab9fe861d179cd
shohamedori <toratakason2003@gmail.com>
2026-06-01 20:29:09 +0900

    draw_img.pyを使ってdatファイルからpbm,pgm画像を作成するコードdat_image.pyの追加

---
+++ script/dat_image.py
@@ -0,0 +1,43 @@
+import os
+import main
+import time
+
+
+#ファイルパス
+python_executable = "C:/Users/torat/Desktop/sotsuken/sotsuken/.venv/Scripts/python.exe"
+draw_img_script = "C:/Users/torat/Desktop/sotsuken/sotsuken/draw_img_gray.py"
+dat_folder = "C:/Users/torat/Desktop/sotsuken/sotsuken/output_weight_350_dat"
+output_folder = "C:/Users/torat/Desktop/sotsuken/sotsuken/True_dat_to_images_350_weight_pgm"
+img_x, img_y = 64,64  # 画像サイズ
+start_time = time.time()
+
+os.makedirs(output_folder, exist_ok=True)
+
+
+#draw_img.py を呼び出すやつ
+def run_draw_img(width, height, data_file, output_file):
+    try:
+        with open(output_file, 'w') as out_file:
+            result = subprocess.run(
+                [python_executable, draw_img_script, str(width), str(height), data_file],
+                stdout=out_file,
+                stderr=subprocess.PIPE,
+                text=True,
+                encoding='utf-8'
+
+            )
+        if result.returncode != 0:
+            print(f"{data_file}でエラー:\n{result.stderr}")
+        else:
+            print(f"{output_file}を出力")
+    except Exception as e:
+        print(f"{data_file}でエラー： {e}")
+
+
+# フォルダ内の座標データの処理
+for fname in os.listdir(dat_folder):
+    if fname.endswith(".dat"):
+        data_file = os.path.join(dat_folder, fname)
+        output_file = os.path.join(output_folder, fname.replace(".dat", ".pgm"))
+        run_draw_img(img_x, img_y, data_file, output_file)
+end_time = time.time()
---
+++ script/draw_img.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+# coding:UTF-8
+# 縦横比保存 グラデーションによるグレースケール化
+# USAGE: ./draw_img.py 画像横サイズ 画像縦サイズ データファイル名
+
+import numpy as np
+import csv
+import sys
+import math
+
+img_x = int(sys.argv[1])  # 画像サイズ：横
+img_y = int(sys.argv[2])  # 画像サイズ：縦
+fname = sys.argv[3]
+
+maxpix = 255  # グレースケール最大階調値
+
+img = np.array([float(img_x), float(img_y)])
+pix = np.zeros((img_x, img_y), dtype=int)  # 画素値（ゼロで初期化）
+
+# 参照、移動の方向
+hear, right, left, up, down, up_right = \
+    [np.array(list) for list in [[0, 0], [1, 0],
+                                 [-1, 0], [0, 1], [0, -1], [1, 1]]]
+
+# def setpic(point, pixv):  # 画素値の設定
+#     # 直線が通過するごとに1だけ増やす
+#     # pix[tuple(point)] = np.min([pix[tuple(point)]+1, maxpix])
+#     # pix[tuple(point)] = 1  # 2値画像
+#     pix[tuple(point)] = pixv  # グラデーション
+
+def drawline(from_p,  dest_p, pixv):  # from_p-dest_pを結ぶ直線を描画する
+    # 画素値 pixv を引数として渡すように改変
+
+    dx, dy = dest_p-from_p
+
+    if dx < 0:  # 終点が第2，3象限にある場合は逆向きに描画
+        dest_p, from_p = from_p, dest_p
+        dx, dy = dest_p-from_p
+
+    n_vec = np.array([dy, -dx])  # 直線の法線ベクトル
+
+    start = np.array([int(np.floor(p)) for p in from_p])  # 移動の出発点
+    endp = np.array([int(np.floor(p)) for p in dest_p])  # 移動の終点
+
+    pix[tuple(start)] = pixv # 出発点の画素値を設定
+
+    movp = np.copy(start)  # 移動点の初期化
+    # 直線が right と up_right の間を通っているとき right に移動、そうでなければmov2に移動
+    # mov2：直線の終点が第1象限にあるとき up、 第4象限にあるとき down
+    mov2 = up if dy >= 0 else down
+
+    while not (np.allclose(movp, endp)):  # 移動点が終点に達するまで繰り返し
+
+        vec1 = movp + right - from_p
+        vec2 = movp + up_right - from_p
+
+        # right と up_right の間を通過
+        if np.dot(n_vec, vec1)*np.dot(n_vec, vec2) <= 0:
+            movp += right
+        else:
+            movp += mov2
+        pix[tuple(movp)] = pixv  # 移動後の画素値を設定
+
+# データファイルの読み込み
+with open(fname, 'r') as f:
+    reader = csv.reader(f,  quoting=csv.QUOTE_NONNUMERIC)
+    dat = np.array([row for row in reader])
+
+# 各軸でスケール変換（縦横比は非保存）
+max, min = np.max(dat, axis=0), np.min(dat, axis=0)  # 各軸に沿った最大と最小
+width, height = max-min  # 画像のオリジナルサイズ
+imgw, imgh = img-1  # 描画領域の横・縦の最大値
+
+#if (rat := np.max([width/imgw, height/imgh])) > 1.0:  # 描画領域を超えている
+rat = np.array([width/imgw, height/imgh])
+dat = dat/rat  # 1/ratにリスケール
+max, min = (max,  min)/rat
+
+# 中心が一致するように平行移動
+mid = (max+min)/2.0
+dat = [row-mid+img/2. for row in dat]
+
+# 描画
+for i in range(len(dat)-1):
+    drawline(dat[i], dat[i+1], math.floor((maxpix+1)/len(dat)*i) )
+    #drawline(dat[i], dat[i+1], 1)
+
+'''
+ 画像データの出力
+'''
+
+# マジックナンバー：P1 2値画像、P2 グレースケール、P3 カラー
+print ("P2\n %d %d\n%d" % (img_x, img_y, maxpix))  # マジックナンバー 横 縦
+#print("P1\n %d %d" % (img_x, img_y))  # マジックナンバー 横 縦
+# 2値画像の場合は maxpix は入れない
+
+# writer = csv.writer(sys.stdout, delimiter=' ', lineterminator='\n')
+for row in np.flipud(pix.T):   # pix を転置して縦方向に反転（配列の並びの関係で必要）
+    print(' '.join(np.array(row, dtype=str)))
+    # print(''.join(np.array((row+1)%2, dtype=str)))  # 反転表示：2値画像
+
+# writer.writerow(row)
+# writer.writerow((row+1)%2)  # 反転表示：2値画像
+# writer.writerow(maxpix-row)  # 反転表示：グレースケール
